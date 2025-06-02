c003332fa33a6fb20bc8f578f11c8587d44bef2a
hernan garcia <hernangarcia615@gmail.com>
2025-06-02 09:06:30 +0200

    Integracion de spring admin con con reconociminto de microservicio con eureka-

---
+++ infraestructuradomain/SpringBootAdmin/src/main/java/com/paymentchain/SpringBootAdmin/SpringBootAdminApplication.java
@@ -0,0 +1,28 @@
+package com.paymentchain.SpringBootAdmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+
+
+
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableScheduling
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
